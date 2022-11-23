8c5cd3a16224d9673ab3270056446c212b83a930
MaheenMohid <117740011+MaheenMohid@users.noreply.github.com>
2022-11-23 16:51:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+const password = document.getElementById('password')
+const background = document.getElementById('background')
+
+password.addEventListener('input', (e) => {
+  const input = e.target.value
+  const length = input.length
+  const blurValue = 20 - length * 2
+  background.style.filter = `blur(${blurValue}px)`
+})
+
+function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+  
